cadb336feb9e4939523a886384101fb5b3649b3a
Tzuyi <a0988426059@gmail.com>
2023-07-27 15:43:27 +0800

    chore: update config store environmental judgment

--- src/stores/modules/config.ts
+++ src/stores/modules/config.ts
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia';
 
 const versionString =
-  import.meta.env.MODE === 'development'
+  import.meta.env.DEV
     ? `${import.meta.env.VITE_APP_VERSION}-dev`
     : import.meta.env.VITE_APP_VERSION;
 
 const defaultState = {
-  isDev: import.meta.env.MODE === 'development',
+  isDev: import.meta.env.DEV,
   version: versionString,
 };
 
