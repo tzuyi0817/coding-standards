e1c91ffcd37287f833e8ba428ccd1b2525e028a8
Tzuyi <a0988426059@gmail.com>
2024-01-08 10:20:11 +0800

    chore: split tsconfig.json file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import IndexPage from '@/pages/IndexPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
