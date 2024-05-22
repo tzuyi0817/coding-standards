019491edfde5a1dc1da31e59ab39008d6b92189d
Tzuyi <a0988426059@gmail.com>
2024-05-22 14:58:03 +0800

    docs: add gitlab ci yml

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      ...defaultTheme.screens,
-    },
     // fontFamily: {
     //   sans: ["Monda", "Noto Sans TC", "sans-serif"],
     // },
