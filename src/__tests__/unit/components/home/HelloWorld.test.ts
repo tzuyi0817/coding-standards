5367979bfbd0d50328efc7d79f7ab8f26024fbde
Tzuyi <a0988426059@gmail.com>
2025-03-21 18:43:04 +0800

    test: fix test file path

--- src/__tests__/unit/components/home/HelloWorld.test.ts
+++ src/__tests__/unit/components/home/HelloWorld.test.ts
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
-import HelloWorld from '@/components/indexPage/HelloWorld.vue';
+import HelloWorld from '@/pages/home/components/HelloWorld.vue';
 import { renderComponent } from '@/__tests__/unit/render';
 import { useConfigStore } from '@/stores';
 import i18n from '@/plugins/i18n';
